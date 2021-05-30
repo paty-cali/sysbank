07e51c37caa8f96bd52a6931b546517fb4e47d7d
Patricia Callizaya <pcs.isabelcallizaya@gmail.com>
2021-05-30 17:13:57 -0400

    Add basic modules templates

---
+++ modules/templates/Clock/clock.global.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h4>
+      {{ time }}
+    </h4>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      interval: null,
+      time: this.$dayjs().format("hh:mm:ss"),
+    };
+  },
+  mounted() {
+    this.interval = setInterval(() => {
+      this.time = this.$dayjs().format("hh:mm:ss");
+    }, 1000);
+  },
+  destroyed() {
+    clearInterval(this.interval);
+  },
+};
+</script>
---
+++ modules/templates/Hello World/hello.global.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hola Mundo
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
