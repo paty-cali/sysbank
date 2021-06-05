949465c4bc7f6c6427d7efdfab7afb8e99878f7a
Patricia Callizaya <pcs.isabelcallizaya@gmail.com>
2021-06-05 06:36:59 -0400

    Remove built files

--- resources/lang/vendor/jdd-api/en/exceptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'ValidationException' => 'The data entered is invalid.',
-    'NotFoundException' => 'Model not found: :model',
-    'InvalidApiCall' => 'Invalid API call.',
-    'AuthorizationException' => 'Unauthorized to :model::action',
-];
--- resources/lang/vendor/jdd-api/es/exceptions.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'ValidationException' => 'Los datos ingresados son inválidos.',
-    'NotFoundException' => 'Modelo no encontrado: :model.',
-    'InvalidApiCall' => 'Llamada API inválida.',
-    'AuthorizationException' => 'No autorizado a :model::action',
-];
