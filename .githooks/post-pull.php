42c98b5ba3536a1568c26701a8ff20020bc96c7a
Patricia Callizaya <pcs.isabelcallizaya@gmail.com>
2021-06-05 06:53:12 -0400

    Config webhook

--- .githooks/post-pull.php
+++ .githooks/post-pull.php
@@ -1,9 +1,9 @@
 <?php
 run(
     // Update libraries
-    onchange(['composer.json'], 'composer install;') .
+    onchange(['composer.lock'], 'composer install --ignore-platform-reqs;') .
     // Update front end
-    onchange(['resources/js','resources/sass','resources/images'], 'npm run prod;') .
+    onchange(['resources/js','resources/css'], 'npm run prod;') .
     // Rebuild database
-    onchange(['database/migrations'], 'composer dumpautoload;php artisan migrate;')
+    onchange(['database/migrations'], 'composer dumpautoload;php artisan migrate --seed;')
 );
